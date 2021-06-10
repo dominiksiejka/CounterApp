3c89a2a02cd41a59d727cf148689cd5879a5cb5a
sieja182 <dev@dominiksiejka.pl>
2021-06-10 18:14:38 +0200

    add the Timer functionality

--- counter.js
+++ counter.js
@@ -9,6 +9,70 @@ window.addEventListener("DOMContentLoaded", () => {
   circle.setAttribute("stroke-dasharray", perimiter);
 
   class Timer {
-    constructor() {}
+    constructor(currentVal, playButton, pouseButton, resetButton, callbacks) {
+      this.currentVal = currentVal;
+      this.playButton = playButton;
+      this.pouseButton = pouseButton;
+      this.resetButton = resetButton;
+
+      if (callbacks) {
+        const { onStart, onStop, onComplete, onReset } = callbacks;
+        this.onStart = onStart;
+        this.onStop = onStop;
+        this.onComplete = onComplete;
+        this.onReset = onReset;
+      }
+      this.playButton.addEventListener("click", this.start);
+      this.pouseButton.addEventListener("click", this.stop);
+      this.resetButton.addEventListener("click", this.reset);
+    }
+    handleRemoveAddListener = (condition) => {
+      if (condition) {
+        this.playButton.removeEventListener("click", this.start);
+      } else {
+        this.playButton.addEventListener("click", this.start);
+      }
+    };
+    start = () => {
+      if (input.value.trim() !== "") {
+        if (this.onStart) {
+          this.onStart(this.timeRemaining);
+        }
+        this.intevralId = setInterval(this.tick, 10);
+        this.handleRemoveAddListener(true);
+      }
+      return;
+    };
+    stop = () => {
+      clearInterval(this.intevralId);
+      if (this.onStop) {
+        this.onStop();
+        this.handleRemoveAddListener(false);
+      }
+    };
+    reset = () => {
+      this.timeRemaining = 0;
+      this.stop();
+      if (this.onReset) {
+        this.onReset();
+      }
+    };
+    tick = () => {
+      if (this.timeRemaining <= 0) {
+        this.stop();
+      } else {
+        this.timeRemaining = this.timeRemaining - 0.01;
+        if (this.onComplete) {
+          this.onComplete(this.timeRemaining);
+        }
+      }
+    };
+
+    get timeRemaining() {
+      return parseFloat(this.currentVal.value);
+    }
+    set timeRemaining(time) {
+      this.currentVal.value = time.toFixed(2);
+    }
   }
 });
