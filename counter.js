b05858a69c9913d697e51edf02c287c05443cde8
sieja182 <dev@dominiksiejka.pl>
2021-06-10 18:07:19 +0200

    query all elements from html file to js file to work with

---
+++ counter.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const playBtn = document.querySelector(".play");
+  const pouseBtn = document.querySelector(".pouse");
+  const input = document.querySelector("input");
+  const circle = document.querySelector("circle");
+  const resetBtn = document.querySelector(".reset");
+});
